d06dd6b65a27b53071ec0ab000fa56b5a762fe1b
ncayiyane <andrewncama@gmail.com>
2025-01-07 09:30:41 +0200

    updated

---
+++ sandbox.js
@@ -0,0 +1,4 @@
+document.querySelector('.menu-toggle').addEventListener('click', function () {
+    const navbar = document.querySelector('.navbar');
+    navbar.classList.toggle('active'); // Toggle the 'active' class
+});
